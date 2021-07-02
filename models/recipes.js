01e656891838870061693ef0de5be3435a62d6ed
gguleid <gguleid@gmail.com>
2021-07-02 13:35:17 -0400

    changes users to customers

--- models/recipes.js
+++ models/recipes.js
@@ -2,12 +2,19 @@ const mongoose = require('require');
 const Schema = mongoose.Schema;
 
 
-const RecipeSchema = new mongoose.Schema({
-    drinkName: String,
-    image: String,
-    name: String,
-    
-});
+const RecipeSchema = new Schema(
+        {
+            drinkName: String,
+            image: String,
+            name: String,
+            createdBy: {
+                type: Schema.Types.ObjectId,
+                required: true,
+                ref: 'User',
+                },
+        }, { timestamps: true },
+);
+            
 
 const Recipe = mongoose.model('Recipe', RecipeSchema);
 
